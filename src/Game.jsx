18eef81e37358c80067b929e62c6762c154261ea
PythonDeveloper <pooja.tarar@gmail.com>
2026-04-03 15:16:27 +0530

    added

--- src/Game.jsx
+++ src/Game.jsx
@@ -3,22 +3,30 @@ import { create } from "zustand";
 import { useShallow } from "zustand/react/shallow";
 import { DOMVisualElement, motion } from "motion/react";
 import { useState, useEffect, useRef } from "react";
-import goblinimg from "./assets/goblin.png";
-import ironclad from "./assets/ironclad.png";
+import goblinimg from "./assets/goblin_new.png";
+import ironclad from "./assets/ironclad_new.png";
+import emberlash from "./assets/emberlash_new.png";
+import swiftshot from "./assets/swiftshot.png";
+import hexblade from "./assets/hexblade.png";
+import ghoststep from "./assets/ghoststep.png";
+import ravanger from "./assets/ravanger.png";
+import nightshade from "./assets/nightshade.png";
+import stoneguard from "./assets/stoneguard.png";
+import dawnkeeper from "./assets/dawnkeeper.png";
 const useGameStore = create((set, get) => ({
   player: { hp: 500 },
   enemy: { hp: 500 },
   playerdeck: [
     { goblin: { attack: 10, defense: 3, hp: 50, img: goblinimg } },
     { ironclad: { attack: 20, defense: 22, hp: 100, img: ironclad } },
-    { emberlash: { attack: 25, defense: 6, hp: 70 } },
-    { swiftshot: { attack: 50, defense: 30, hp: 10 } },
-    { hexblade: { attack: 200, defense: 50, hp: 200 } },
-    { ghoststep: { attack: 40, defense: 5, hp: 50 } },
-    { ravanger: { attack: 100, defense: 45, hp: 50 } },
-    { nightshade: { attack: 30, defense: 12, hp: 30 } },
-    { stoneguard: { attack: 5, defense: 200, hp: 50 } },
-    { dawnkeeper: { attack: 70, defense: 50, hp: 30 } },
+    { emberlash: { attack: 25, defense: 6, hp: 70, img: emberlash } },
+    { swiftshot: { attack: 50, defense: 30, hp: 10, img: swiftshot } },
+    { hexblade: { attack: 200, defense: 50, hp: 200, img: hexblade } },
+    { ghoststep: { attack: 40, defense: 5, hp: 50, img: ghoststep } },
+    { ravanger: { attack: 100, defense: 45, hp: 50, img: ravanger } },
+    { nightshade: { attack: 30, defense: 12, hp: 30, img: nightshade } },
+    { stoneguard: { attack: 5, defense: 200, hp: 50, img: stoneguard } },
+    { dawnkeeper: { attack: 70, defense: 50, hp: 30, img: dawnkeeper } },
   ],
   enemydeck: [
     { dreadlord: { hp: 32, attack: 50, defense: 20 } },
@@ -86,25 +94,30 @@ function Game() {
 
   return (
     <>
-      <div>
-        {player_hand.map((e) => (
-          <>
-            <div className="card w-50 border-amber-500 flex flex-col items-center border-5">
-              <img src={e[Object.keys(e)[0]].img} className="w-60" />
-              <h1 className="text-amber-200 text-3xl bg-yellow-700 text-center rounded-xl">
-                {Object.keys(e)[0].toLocaleUpperCase()}
-              </h1>
-              <div className="bg-yellow-100 w-40 flex items-center flex-col">
-                <h2 className="text-stone-900 text-3xl w-fit">
-                  ATK: {e[Object.keys(e)[0]].attack}
-                </h2>
-                <h2 className="text-stone-900 text-3xl w-fit">
-                  DEF: {e[Object.keys(e)[0]].defense}
-                </h2>
+      <div className="flex items-end gap-5">
+        <div>
+          {player_hand.map((e) => (
+            <>
+              <div className="card w-50 border-amber-500 flex flex-col items-center border-5">
+                <img src={e[Object.keys(e)[0]].img} className="w-60 h-31" />
+                <h1 className="text-amber-200 text-3xl bg-yellow-700 text-center rounded-xl">
+                  {Object.keys(e)[0].toLocaleUpperCase()}
+                </h1>
+                <div className="bg-yellow-100 w-40 flex items-center flex-col">
+                  <h2 className="text-stone-900 text-3xl w-fit">
+                    ATK: {e[Object.keys(e)[0]].attack}
+                  </h2>
+                  <h2 className="text-stone-900 text-3xl w-fit">
+                    DEF: {e[Object.keys(e)[0]].defense}
+                  </h2>
+                </div>
               </div>
-            </div>
-          </>
-        ))}
+            </>
+          ))}
+        </div>
+        <div className="card_deck w-50 border-amber-500 flex items-center flex-col justify-center border-5 text-5xl h-61 ">
+          ⭐
+        </div>
       </div>
     </>
   );
